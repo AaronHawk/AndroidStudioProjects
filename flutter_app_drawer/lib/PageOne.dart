711aa9acfe8a4e9d16ddca567142683b7a970d0c
MagickRitual <56135406+MagickRitual@users.noreply.github.com>
2019-10-10 15:23:57 -0600

    Update PageOne.dart

--- flutter_app_drawer/lib/PageOne.dart
+++ flutter_app_drawer/lib/PageOne.dart
@@ -1,7 +1,7 @@
 //PageOne.dart
 import 'package:flutter/material.dart';
 
-class PageOne extends StatefulWidget{
+class PageOne extends StatefulWidget {
   @override
   PageOneState createState() => HomePageState();
 
@@ -13,14 +13,13 @@ PageOneState extends State<PageOne>{
 String mainProfilePix = "http://wwwnc.fvcc.edu/fileadmin/photos/people/james_goudy.jpg";
 String minProfilePix = "https://cdn.hswstatic.com/gif/pug.jpg";
 
-void switchUser(){
+void switchUser() {
   String backupString = mainProfilePix;
-  this.setState((){
+  this.setState(() {
     mainProfilePix = minProfilePix;
     minProfilePix = backupString;
   });
 }
-
 
 
 }
