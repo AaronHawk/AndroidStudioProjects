ae7256ff20311c546cddf9b91cdadbd880c940a7
MagickRitual <56135406+MagickRitual@users.noreply.github.com>
2019-10-29 14:04:54 -0600

    10/29

---
+++ flutter_app_mysqljockey/android/app/src/main/java/shiningson/flutter_app_mysqljockey/MainActivity.java
@@ -0,0 +1,13 @@
+package shiningson.flutter_app_mysqljockey;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
